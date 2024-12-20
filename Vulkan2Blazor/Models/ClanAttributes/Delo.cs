a7e87b3f70e7994bc40076d8f86327deaf4bf548
Privatech38 <ziga.slatnar.stagar@gmail.com>
2024-12-20 18:53:07 +0100

    Added delo model

---
+++ Vulkan2Blazor/Models/ClanAttributes/Delo.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vulkan2Blazor.Models.ClanAttributes;
+
+public class Delo
+{
+    public int DeloId { get; set; }
+    
+    [Required]
+    public Clan Clan { get; set; }
+    
+    [Required]
+    public string VrstaDela { get; set; }
+    
+    [Required]
+    public DateOnly Datum { get; set; }
+    
+    [Required]
+    public float SteviloUr { get; set; }
+    
+}
