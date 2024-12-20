d0b91fd0ca257629ab6f1cf94d95340b561d10b2
Privatech38 <ziga.slatnar.stagar@gmail.com>
2024-12-20 18:13:40 +0100

    Updated Clan with annotations and added address

--- Vulkan2Blazor/Models/Clan.cs
+++ Vulkan2Blazor/Models/Clan.cs
@@ -2,21 +2,33 @@
 
 namespace Vulkan2Blazor.Models;
 
+public enum Spol {
+    MOSKI,
+    ZENSKA
+}
+
 public class Clan
 {
     public int ClanId { get; set; }
     
+    [Required]
     public string Ime { get; set; }
     
+    [Required]
     public string Priimek { get; set; }
     
+    [Required]
     public DateOnly DatumRojstva { get; set; }
     
-    public string? Spol { get; set; }
+    [Required]
+    public Spol Spol { get; set; }
     
+    [Required]
+    [RegularExpression(@"^\d{13}$", ErrorMessage = "EMŠO mora biti dolgo točno 13 števk.")]
     public string EMSO { get; set; }
     
     // Drustvo
+    [Required]
     public GasilskoDrustvo GasilskoDrustvo { get; set; }
     
     // Cini
@@ -25,11 +37,29 @@ public class Clan
     // Članstva
     public ICollection<Clanstvo> Clanstva { get; }
     
+    // Naslov
+    [Required]
+    public string Naslov { get; set; }
+        
+    [Required]
+    [RegularExpression(@"^\d{4}$", ErrorMessage = "Poštna številka mora biti dolga točno 4 števke.")]
+    public string Posta { get; set; }
+        
+    [Required]
+    public string Kraj { get; set; }
+    
     // Kontakt
     [DataType(DataType.EmailAddress)]
+    [EmailAddress]
     public string? Email { get; set; }
     
     [DataType(DataType.PhoneNumber)]
+    [Phone]
     public string? Telefon { get; set; }
+    
+    // Podatki
+    
+    [RegularExpression(@"^\d{8}$", ErrorMessage = "Davčna številka mora biti dolga točno 8 števk.")]
+    public string? DavcnaStevilka { get; set; }
     
 }
