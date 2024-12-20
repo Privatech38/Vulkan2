5bb1324c8690f67d13fe7c5380e1b02459c19c30
Privatech38 <ziga.slatnar.stagar@gmail.com>
2024-12-20 18:14:04 +0100

    Updated zveza to include necessary data

--- Vulkan2Blazor/Models/GasilskaZveza.cs
+++ Vulkan2Blazor/Models/GasilskaZveza.cs
@@ -6,30 +6,41 @@ public class GasilskaZveza
 {
     public int GasilskaZvezaId { get; set; }
     
+    [Required]
     public string Naziv { get; set; }
     
     // Naslov
-    public string? Naslov { get; set; }
+    [Required]
+    public string Naslov { get; set; }
     
-    public string? Posta { get; set; }
+    [Required]
+    [RegularExpression(@"^\d{4}$", ErrorMessage = "Poštna številka mora biti dolga točno 4 števke.")]
+    public string Posta { get; set; }
     
-    public string? Kraj { get; set; }
+    [Required]
+    public string Kraj { get; set; }
     
     // Kontakt
     [DataType(DataType.EmailAddress)]
+    [EmailAddress]
     public string? Email { get; set; }
     
     [DataType(DataType.PhoneNumber)]
+    [Phone]
     public string? Telefon { get; set; }
     
     [DataType(DataType.Url)]
+    [Url]
     public string? SpletnaStran { get; set; }
     
     // Podatki
-    public string? MaticnaStevilka { get; set; }
-    
-    public string? DavcnaStevilka { get; set; }
-    
+    [Required]
+    [RegularExpression(@"^\d{10}$", ErrorMessage = "Matična številka mora biti dolga točno 10 števk.")]
+    public string MaticnaStevilka { get; set; }
+        
+    [Required]
+    [RegularExpression(@"^\d{8}$", ErrorMessage = "Davčna številka mora biti dolga točno 8 števk.")]
+    public string DavcnaStevilka { get; set; }
     
     public ICollection<GasilskoDrustvo> GasilskaDrustva { get; }
     
