8315bb7fbc7fb50977933ad77a6c3bb415b286a5
Privatech38 <ziga.slatnar.stagar@gmail.com>
2024-12-20 15:13:19 +0100

    Updated GasilskaZveza.cs model to include necessary data.

--- Vulkan2Blazor/Models/GasilskaZveza.cs
+++ Vulkan2Blazor/Models/GasilskaZveza.cs
@@ -1,8 +1,35 @@
-﻿namespace Vulkan2Blazor.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vulkan2Blazor.Models;
 
 public class GasilskaZveza
 {
     public int GasilskaZvezaId { get; set; }
+    
+    public string Naziv { get; set; }
+    
+    // Naslov
+    public string? Naslov { get; set; }
+    
+    public string? Posta { get; set; }
+    
+    public string? Kraj { get; set; }
+    
+    // Kontakt
+    [DataType(DataType.EmailAddress)]
+    public string? Email { get; set; }
+    
+    [DataType(DataType.PhoneNumber)]
+    public string? Telefon { get; set; }
+    
+    [DataType(DataType.Url)]
+    public string? SpletnaStran { get; set; }
+    
+    // Podatki
+    public string? MaticnaStevilka { get; set; }
+    
+    public string? DavcnaStevilka { get; set; }
+    
     
     public ICollection<GasilskoDrustvo> GasilskaDrustva { get; }
     
