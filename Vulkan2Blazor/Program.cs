8b2bfc5167f0b6e5f79862fc256a0d542135b18b
Privatech38 <ziga.slatnar.stagar@gmail.com>
2024-12-28 20:36:10 +0100

    Fixed is development checker in Program.cs

--- Vulkan2Blazor/Program.cs
+++ Vulkan2Blazor/Program.cs
@@ -42,12 +42,15 @@ builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSe
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
