7e602abf35c775113228bb9a17bc796a61db3569
Privatech38 <ziga.slatnar.stagar@gmail.com>
2024-12-29 17:28:05 +0100

    Updated program cs to include identity roles

--- Vulkan2Blazor/Program.cs
+++ Vulkan2Blazor/Program.cs
@@ -33,6 +33,7 @@ builder.Services.AddQuickGridEntityFrameworkAdapter();
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<Vulkan2Context>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
