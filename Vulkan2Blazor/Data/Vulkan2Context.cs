db95a350885432001217930e00383dbd8de79cb9
Privatech38 <ziga.slatnar.stagar@gmail.com>
2025-01-12 22:38:24 +0100

    Added CRUD pages for zdravstveni pregledi

--- Vulkan2Blazor/Data/Vulkan2Context.cs
+++ Vulkan2Blazor/Data/Vulkan2Context.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Vulkan2Blazor.Models;
+using Vulkan2Blazor.Models.ClanAttributes;
 
 namespace Vulkan2Blazor.Data
 {
@@ -18,5 +19,6 @@ namespace Vulkan2Blazor.Data
         public DbSet<Vulkan2Blazor.Models.Clan> Clan { get; set; } = default!;
         public DbSet<Vulkan2Blazor.Models.GasilskoDrustvo> GasilskoDrustvo { get; set; } = default!;
         public DbSet<Vulkan2Blazor.Models.GasilskaZveza> GasilskaZveza { get; set; } = default!;
+        public DbSet<Vulkan2Blazor.Models.ClanAttributes.ZdravstveniPregled> ZdravstveniPregled { get; set; } = default!;
     }
 }
